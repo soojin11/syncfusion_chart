0f3def7c9287aa36ab2e123b420d8a293e4ba050
soojin <soojin@wgslabs.com>
2021-10-19 13:30:30 +0900

    Second

--- lib/function/database_helper.dart
+++ lib/function/database_helper.dart
@@ -1,80 +1,93 @@
-// import 'dart:io';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common/sqlite_api.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-// import 'package:path/path.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:sqflite/sqflite.dart';
+class DatabaseHelper {
+  static final _databaseName = "config_wr.db";
+  static final _databaseVersion = 1;
+  static final table = 'info';
+  static final name = 'Name';
+  static final value = 'Value';
 
-// class DatabaseHelper {
-//   //초기화 전에 변수 만들어주기
-//   static final _dbName = 'myDatabase.db';
-//   static final _dbVersion = 1;
-//   static final _tableName = 'myTable';
-//   //table에 들어갈 column 설정
-//   static final columnName = 'name';
-//   static final columnId = '_id';
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  var databaseFactory = databaseFactoryFfi;
+  static Database? db;
+  Future<Database?> get database async {
+    // print("database");
+    if (db != null) return db;
+    db = await _initDatabase();
+    return db;
+  }
 
-//   //싱글톤 패턴으로 만들어 인스턴스 1개만 생성, lifecycle 1개
-//   DatabaseHelper._privateConstructor();
-//   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    db = await databaseFactory.openDatabase(path,
+        options: OpenDatabaseOptions(
+            onCreate: _onCreate, version: _databaseVersion));
+    if (db == null) print('_initDatabase db가 널이네');
+    return db!;
+  }
 
-//   //initailized Database, add a function, 초기화 전에 변수 생성
-//   static Database? _database;
+  Future _onCreate(Database db, int version) async {
+    await db.execute("CREATE TABLE info (Name String, Value INTEGER)");
+    List<Map<String, dynamic>> values = [];
+    for (var i = 0; i < 5; i++) {
+      values.add({
+        "name": "$i-start",
+        "value": 1,
+      });
+      values.add({
+        "name": "$i-end",
+        "value": 1,
+      });
+    }
+    values.add({
+      "name": "integrationTime",
+      "value": 20,
+    });
+    values.add({
+      "name": "interval",
+      "value": 100,
+    });
+    values.add({
+      "name": "sampling",
+      "value": 500,
+    });
+    values.add({
+      "name": "serialPort",
+      "value": "COM1",
+    });
+    values.add({
+      "name": "hemos",
+      "value": 1,
+    });
+    values.add({
+      "name": "oes",
+      "value": 1,
+    });
+    for (var i = 0; i < values.length; i++) {
+      Map<String, dynamic> row = {
+        name: values[i]["name"],
+        value: values[i]["value"],
+      };
+      await db.insert(table, row);
+    }
+  }
 
-//   //처음엔 다 null이니까
-//   Future<Database> get database async {
-//     if (_database != null) {
-//       return _database!;
-//     } else {
-//       _database = await _initiateDatabase();
-//     }
-//     return _database!;
-//   }
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    Database? database = await instance.database;
+    return await database!.query(table);
+  }
 
-//   //database가 null일때
-//   _initiateDatabase() async {
-//     Directory directory = await getApplicationDocumentsDirectory();
-//     //파일 이름(_dbName)이랑 directory를 join 해주는거
-//     String path = join(directory.path, _dbName);
-//     //open, 어떤 버전을 사용하는건지 쫓아가려고 계속 써줌(우린 어차피 버전 1개)
-//     ////onCreate: database가 생성되면 뭘 할지, 보통 테이블을 넣지, onCreate는 Database랑 int return함
-//     return await openDatabase(path, version: _dbVersion, onCreate: _onCreate);
-//   }
-
-//   Future _onCreate(Database db, int version) {
-//     //query 생성, table 이름, row, colum 설정해주는 것
-//     db.execute(''' 
-//       CREATE TABLE $_tableName($columnId INTEGER PRIMARY KEY,
-//       $columnName TEXT NOT NULL)
-//       ''');
-//       return _onCreate(db, version);
-//   }
-
-//   //query table 에 내가 추가할 것 Map type으로
-//   //{"_id":12, "name":"Saheb"} 이런 형태로 Map이니까
-//   Future<int> insert(Map<String, dynamic> row) async {
-//     //instance에서 database를 가져올 때까지 기다려라
-//     Database db = await instance.database;
-//     return await db.insert(_tableName, row);
-//   }
-
-//   //list type의 Map type의 string, dynamic type을 return
-//   Future<List<Map<String, dynamic>>> queryAll() async {
-//     Database db = await instance.database;
-//     return await db.query(_tableName);
-//   }
-
-//   //어떤 id를 업데이트 할지 알아야하니까 Map, row에 전달
-//   //update하면 row가 바뀌니까 where 사용 columnId에 Id parsing, name은 바꿀필요 없자나
-//   //where에 지정할 조건 값들이 많을 때 조건 값을 물음표로 대체, ?에 들어갈 값은 whereArgs: []에 지정
-//   Future<int> update(Map<String, dynamic> row) async {
-//     Database db = await instance.database;
-//     int id = row[columnId];
-//     return await db
-//         .update(_tableName, row, where: '$columnId = ?', whereArgs: [id]);
-//   }
-
-//   Future<int> delete(int id) async {
-//     Database db = await instance.database;
-//     return await db.delete(_tableName, where: '$columnId = ?', whereArgs: [id]);
-//   }
-// }
+  Future<int> update(Map<String, dynamic> row) async {
+    Database? database = await instance.database;
+    String name = row["Name"];
+    return await database!
+        .update(table, row, where: 'Name = ?', whereArgs: [name]);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:math' as math;
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
@@ -11,7 +16,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -34,8 +39,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late List<SpecData> _chartData;
   late List<SpecData> _hiData;
+  late List<SpecData> _oneData;
   late ChartSeriesController _chartSeriesController;
   late ChartSeriesController _chartSeriesControllerr;
+  late ChartSeriesController _chartSeriesControllerrr;
   late ZoomPanBehavior _zoomPanBehavior;
   //late SfCartesianChart chart;
 
@@ -43,31 +50,84 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     _chartData = getChartData();
     _hiData = getHiData();
+    _oneData = getOneData();
     Timer.periodic(const Duration(seconds: 1), updateDataSource);
-    super.initState();
     _zoomPanBehavior = ZoomPanBehavior(
         enableSelectionZooming: true,
         selectionRectBorderColor: Colors.red,
         selectionRectBorderWidth: 1,
         selectionRectColor: Colors.grey,
         enableDoubleTapZooming: true,
+        enableMouseWheelZooming: true,
         enablePinching: true,
         enablePanning: true);
+
+    super.initState();
   }
+
+  // @override
+  // void dispose() {
+  //   _timer.cancel();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+      appBar: AppBar(
+        title: Text('WR'),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text('config'),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
       body: Column(
         children: [
+          Container(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              OutlinedButton(
+                  onPressed: () {
+                    Get.off(() => MyHomePage(title: 'WR'));
+                  },
+                  child: Text('Reload')),
+              OutlinedButton(
+                  onPressed: () async {
+                    await Get.find<CountControllerWithReactive>().csvSaveInit();
+                    Get.find<CountControllerWithReactive>().bFileSave.value =
+                        true;
+                  },
+                  child: Text('Save File')),
+              OutlinedButton(onPressed: () {}, child: Text('Start')),
+              OutlinedButton(onPressed: () {}, child: Text('Stop')),
+            ],
+          ),
           SfCartesianChart(
+            legend: Legend(
+                isVisible: true,
+                toggleSeriesVisibility: true,
+                position: LegendPosition.top),
             zoomPanBehavior: _zoomPanBehavior,
             primaryXAxis:
                 NumericAxis(autoScrollingMode: AutoScrollingMode.start),
             title: ChartTitle(text: '왜 안돼'),
             series: <ChartSeries<SpecData, int>>[
               SplineSeries(
+                name: 'Num1',
+                // trendlines: <Trendline>[
+                //   Trendline(
+                //       type: TrendlineType.logarithmic, color: Colors.green)
+                // ],
+                enableTooltip: true,
                 onRendererCreated: (ChartSeriesController controller) {
                   _chartSeriesController = controller;
                 },
@@ -76,17 +136,33 @@ class _MyHomePageState extends State<MyHomePage> {
                 yValueMapper: (SpecData spec, _) => spec.num,
               ),
               SplineSeries(
+                name: 'Num2',
+                // trendlines: <Trendline>[
+                //   Trendline(type: TrendlineType.logarithmic, color: Colors.red)
+                // ],
+                enableTooltip: true,
                 onRendererCreated: (ChartSeriesController controller) {
                   _chartSeriesControllerr = controller;
                 },
                 dataSource: _hiData,
                 xValueMapper: (SpecData spec, _) => spec.time,
                 yValueMapper: (SpecData spec, _) => spec.num,
+              ),
+              SplineSeries(
+                name: 'Num3',
+                // trendlines: <Trendline>[
+                //   Trendline(type: TrendlineType.logarithmic, color: Colors.red)
+                // ],
+                enableTooltip: true,
+                onRendererCreated: (ChartSeriesController controller) {
+                  _chartSeriesControllerrr = controller;
+                },
+                dataSource: _oneData,
+                xValueMapper: (SpecData spec, _) => spec.time,
+                yValueMapper: (SpecData spec, _) => spec.num,
               )
             ],
           ),
-          TextButton(onPressed: () {}, child: Text('Save File'))
-          //Container(width: 100,height: 30, color: Colors.amber, child: TextButton(child: ,))
         ],
       ),
     ));
@@ -94,32 +170,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<SpecData> getChartData() {
     final List<SpecData> _chartData = [
-      SpecData(time: 0, num: 5),
-      SpecData(time: 1, num: 2),
-      SpecData(time: 2, num: 7)
+      // SpecData(time: 0, num: 5),
+      // SpecData(time: 1, num: 2),
+      // SpecData(time: 2, num: 7)
     ];
     return _chartData;
   }
 
   List<SpecData> getHiData() {
     final List<SpecData> _hiData = [
-      SpecData(time: 0, num: 1),
-      SpecData(time: 1, num: 7),
-      SpecData(time: 2, num: 4)
+      // SpecData(time: 0, num: 1),
+      // SpecData(time: 1, num: 7),
+      // SpecData(time: 2, num: 4)
     ];
     return _hiData;
   }
 
-  int time = 3;
+  List<SpecData> getOneData() {
+    final List<SpecData> _oneData = [
+      // SpecData(time: 0, num: 1),
+      // SpecData(time: 1, num: 7),
+      // SpecData(time: 2, num: 4)
+    ];
+    return _oneData;
+  }
+
+  int time = 0;
+  int timee = 0;
+  int timeee = 0;
   void updateDataSource(Timer timer) {
     _chartData.add(SpecData(time: time++, num: math.Random().nextInt(50)));
+    //_chartData.removeAt(0);
     _chartSeriesController.updateDataSource(
       addedDataIndex: _chartData.length - 1,
+      //removedDataIndex: 0,
     );
-    _hiData.add(SpecData(time: time++, num: math.Random().nextInt(50)));
+    _hiData.add(SpecData(time: timee++, num: math.Random().nextInt(50)));
+    //_chartData.removeAt(0);
     _chartSeriesControllerr.updateDataSource(
-      addedDataIndex: _chartData.length - 1,
+      addedDataIndex: _hiData.length - 1,
+      //removedDataIndex: 0,
     );
+    _oneData.add(SpecData(time: timeee++, num: math.Random().nextInt(50)));
+    _chartSeriesControllerrr.updateDataSource(
+        addedDataIndex: _oneData.length - 1);
   }
 }
 
@@ -128,3 +222,80 @@ class SpecData {
   final int num;
   SpecData({required this.time, required this.num});
 }
+
+//getHiData, getChartData를 받아서 저장해야 하니까,,,,
+class CountControllerWithReactive extends GetxController {
+  static CountControllerWithReactive get to => Get.find();
+  RxString filePath = ''.obs;
+  RxBool bFileSave = false.obs;
+
+  Future<File> csvSave() async {
+    DateTime current = DateTime.now();
+    final String fileNameDate =
+        '${DateFormat('yyyy-MM-dd HH:mm:ss').format(current)}';
+    print('fileNameDate $fileNameDate');
+    List<dynamic> getHiData = [];
+    List<List<dynamic>> addGetHiData = [];
+    //var filePath;
+    File file = File(filePath.value);
+    getHiData.add(fileNameDate);
+    addGetHiData.add(getHiData);
+    String csv = const ListToCsvConverter().convert(addGetHiData) + '\n';
+    return file.writeAsString(csv, mode: FileMode.append);
+  }
+
+  Future<File> csvSaveInit() async {
+    DateTime current = DateTime.now();
+    final String fileNameDate =
+        '${DateFormat('yyyy-MM-dd HH:mm:ss').format(current)}';
+    await Directory('datafiles').create();
+    filePath.value = "./datafiles/$fileNameDate.csv";
+    File file = File(filePath.value);
+
+    String firstRow = "Test";
+    String secondRow = "Hello";
+    String helloTest = firstRow + secondRow;
+
+    return file.writeAsString(helloTest);
+  }
+}
+
+
+
+// class GenerateCSV {
+//   static generateCsv(List<List<String>> data, String fileName) async {
+//     String csvData = ListToCsvConverter().convert(data);
+//     Directory? _appDocDir = await getExternalStorageDirectory();
+//     String newPath = "";
+//     print(_appDocDir);
+//     List<String> paths = _appDocDir!.path.split("/");
+//     for (int x = 1; x < paths.length; x++) {
+//       String folder = paths[x];
+//       if (folder != "Android") {
+//         newPath += "/" + folder;
+//       } else {
+//         break;
+//       }
+//     }
+//     newPath = newPath + "/RPSApp";
+//     _appDocDir = Directory(newPath);
+
+//     final Directory _appDocDirFolder = Directory(
+//         '${_appDocDir.path}/Covid tracker/');
+
+//     if (await _appDocDirFolder
+//         .exists()) { //if folder already exists return path
+//       return _appDocDirFolder.path;
+//     } else { //if folder not exists create folder and then return its path
+//       final Directory _appDocDirNewFolder = await _appDocDirFolder.create(
+//           recursive: true);
+
+
+//       final path = "${_appDocDirNewFolder.path}/$fileName";
+//       print(path);
+//       final File file = File(path);
+//       await file.writeAsString(csvData);
+//       print("File created");
+//     }
+//   }
+// }
